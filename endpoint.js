9988f936c16eabc0a3e7285a293c1db2bdef524c
lexmanthefirst <alex.okhitoya@lifesci.uniben.edu>
2023-09-10 07:31:45 -0400

    minor code change

--- endpoint.js
+++ endpoint.js
@@ -31,4 +31,7 @@ app.get("/endpoint", (req, res) => {
   res.json(response);
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`Server running on port http://localhost:${PORT}`)
+);
